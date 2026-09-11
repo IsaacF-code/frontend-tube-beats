365527784a1ef3cb61d7610af546b31b13632153
Isaac Freires <isaacfreires@gmail.com>
2026-09-11 01:34:43 -0300

    feat: implement video search functionality with API integration
    - feat: show data of search

--- tube-beats/src/App.tsx
+++ tube-beats/src/App.tsx
@@ -1,8 +1,31 @@
 import { useState } from 'react';
 
+type VideoInfo = {
+  title: string;
+  duration: number;
+  thumbnail: string;
+};
+
 function App() {
   const [url, setUrl] = useState('');
+  const [video, setVideo] = useState<VideoInfo | null>(null);
   
+  async function handleSearch() {
+   const response = await fetch("http://localhost:3000/api/video/info", {
+     method: "POST",
+     headers: {
+       "Content-Type": "application/json",
+     },
+     body: JSON.stringify({
+       url: url,
+     })
+   })
+   const data = await response.json();
+  
+   console.log("React: ", data);
+
+   setVideo(data.data);
+  } 
   return (
     <div className="App">
       <h1>Tube Beats</h1>
@@ -15,24 +38,17 @@ function App() {
       <button onClick={handleSearch}>
         Buscar
       </button>
+
+      {video && (
+        <div>
+          <h2>{video.title}</h2>
+
+          <p>Duração: {video.duration} segundos</p>
+          <img src={video.thumbnail} alt={video.title} />
+        </div>
+      )}
     </div>
   );
-  
-  async function handleSearch() {
-    const response = await fetch("http://localhost:3000/api/video/info", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        url: url,
-      })
-    })
-    const data = await response.json();
-  
-    console.log("React: ", data);
-  } 
-
 }
 
 export default App;
